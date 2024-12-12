3f3c50fe6d35de7243840b27a3f46b2dbdc6a0cc
Jeyshel <149998953+Jeyshel@users.noreply.github.com>
2024-12-12 11:46:44 +0800

    student controller

--- demo/src/main/java/com/example/demo/student/Student.java
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -10,12 +10,89 @@ public class Student {
     private LocalDate birthDate;
     private String email;
 
-    public Student(long id, String firstName, String middleName, String lastName, LocalDate birthDate, String email) {
+    public Student(long id,
+                   String firstName,
+                   String middleName,
+                   String lastName,
+                   LocalDate birthDate,
+                   String email) {
         this.id = id;
         this.firstName = firstName;
         this.middleName = middleName;
         this.lastName = lastName;
         this.birthDate = birthDate;
         this.email = email;
+    }
+
+    public Student(String firstName,
+                   String middleName,
+                   String lastName,
+                   LocalDate birthDate,
+                   String email) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
